58a6b3586483ba7ec06d4130a8b381bfc2d8442e
Darikson <dariksoma@gmail.com>
2025-05-07 11:27:26 -0500

    feat: add dotenv

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const { Pool } = require('pg');
 const cors = require('cors');
+require('dotenv').config(); // ← Importante
 
 const app = express();
 app.use(cors());
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'sige360',
-  password: 'Anyosa19',
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 app.get('/api/mensajes', async (req, res) => {
